94ef6b30bc1255cba782d414da17a422b15b4997
Chakrapani Upadhyaya <source.lastbenchcoder@gmail.com>
2022-08-21 18:19:26 +0530

    path changed

--- src/components/constants/AppConst.js
+++ src/components/constants/AppConst.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://cwc-training-manager-api.herokuapp.com/api"
-//export const IMAGE_URL = "https://cwc-training-manager-api.herokuapp.com/images"
+export const BASE_URL = "https://cwc-training-manager-api.herokuapp.com/api"
+export const IMAGE_URL = "https://cwc-training-manager-api.herokuapp.com/images"
 
-export const BASE_URL = "http://localhost:9999/api"
-export const IMAGE_URL = "http://localhost:9999/images"
+//export const BASE_URL = "http://localhost:9999/api"
+//export const IMAGE_URL = "http://localhost:9999/images"
